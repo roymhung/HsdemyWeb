b7e64d9bd04ab0d86553a5fada2f290540430fbd
royhung <lemanhhung1555@gmail.com>
2026-01-06 18:23:01 +0700

    JSTL trong view

--- src/main/java/Hsdemy/vn/HsdemyWeb/controller/UserController.java
+++ src/main/java/Hsdemy/vn/HsdemyWeb/controller/UserController.java
@@ -1,32 +1,30 @@
 package Hsdemy.vn.HsdemyWeb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import Hsdemy.vn.HsdemyWeb.service.UserService;
 
-// @RestController
-// public class UserController {
-
-// private UserService userService;
-
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// };
-// }
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/")
-    public String getHomePage() {
-        return "hello";
+    private UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/") // user: vao website
+    public String getHomePage(Model model) {// controller
+        String test = this.userService.handleHello();// model: goi toi model xu ly du lieu
+        model.addAttribute("roy", test); // sau khi model co data gui lai cho controller
+        model.addAttribute("royhung", "test h2 from controller with model"); // controller xu li
+                                                                             // data roi gui lai cho
+                                                                             // view
+        return "hello"; // view: render ra du lieu roi gui lai cho user
     };
 }
 
